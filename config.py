bc5f2d06c63f2901d6650b80b3f130507fa40f39
Badnam xd <yadavxd746@gmail.com>
2025-03-07 05:05:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/legendtosif/loda",
+    "https://github.com/Badnam019/Clone",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
